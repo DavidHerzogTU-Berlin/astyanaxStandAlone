e3e9b8e1d07cd6af1d9a4b26e354d2d013dee753
freecookie <david.herzog@campus.tu-berlin.de>
2014-11-19 17:22:50 +0100

    commented out the junit test

--- astyanax-client/src/test/java/com/c3/astyanax/AstyanaxClientTest.java
+++ astyanax-client/src/test/java/com/c3/astyanax/AstyanaxClientTest.java
@@ -14,15 +14,22 @@ import java.util.Iterator;
 public class AstyanaxClientTest {
 	@Test
 	public void insertReadDeleteTest() {
-		AstyanaxClient ac1 = new AstyanaxClient();
+		/**AstyanaxClient ac1 = new AstyanaxClient();
 		if(ac1.init()) {
+			System.out.println("Init worked");
 			HashMap<String, String> values = new HashMap<String, String>();
 			values.put("age", "57");
 	    	values.put("middlename", "bradley");
 	    	values.put("favoritecolor", "blue");
 	    	assertTrue(ac1.insert("data","HansBradley", values));
+	    	HashMap<String, String> result = new HashMap<String, String>();
+	    	assertTrue( ac1.read("data", "HansBradley", null, result));
+	    	System.out.println("Results for HansBradley coming: ");
+			for (String column : result.keySet()) {
+				System.out.println("Column: " + column + " Value: " + result.get(column) );
+			}
 		} else {
 			System.out.println("Init failed"); 
-		}
+		}**/
 	}
 }

d72d12d5db222fb2a8dca2b27268f9bdd0f46dbc
freecookie <david.herzog@campus.tu-berlin.de>
2014-11-19 16:21:29 +0100

    init now set

--- astyanax-client/src/main/java/com/c3/astyanax/AstyanaxClient.java
+++ astyanax-client/src/main/java/com/c3/astyanax/AstyanaxClient.java
@@ -1,8 +1,49 @@
 package com.c3.astyanax;
 
-import com.netflix.astyanax.model.ColumnFamily;
-import com.netflix.astyanax.Keyspace;
+import static com.netflix.astyanax.examples.ModelConstants.*;
+
 import com.netflix.astyanax.AstyanaxContext;
+import com.netflix.astyanax.ColumnListMutation;
+import com.netflix.astyanax.Keyspace;
+import com.netflix.astyanax.MutationBatch;
+import com.netflix.astyanax.connectionpool.NodeDiscoveryType;
+import com.netflix.astyanax.connectionpool.OperationResult;
+import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
+import com.netflix.astyanax.connectionpool.impl.*;
+import com.netflix.astyanax.connectionpool.impl.ConnectionPoolConfigurationImpl;
+import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
+import com.netflix.astyanax.model.Column;
+import com.netflix.astyanax.model.ColumnFamily;
+import com.netflix.astyanax.model.ColumnList;
+import com.netflix.astyanax.model.CqlResult;
+import com.netflix.astyanax.model.Row;
+import com.netflix.astyanax.serializers.IntegerSerializer;
+import com.netflix.astyanax.serializers.StringSerializer;
+import com.netflix.astyanax.thrift.ThriftFamilyFactory;
+import com.netflix.astyanax.thrift.ThriftColumnFamilyQueryImpl;
+import com.netflix.astyanax.query.IndexQuery;
+import com.netflix.astyanax.model.ConsistencyLevel;
+import com.netflix.astyanax.model.Rows;
+import java.net.InetAddress;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.nio.ByteBuffer;
+import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Properties;
+import java.io.UnsupportedEncodingException;
+import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import com.netflix.astyanax.connectionpool.impl.PendingRequestMap;
+import com.netflix.astyanax.connectionpool.exceptions.OperationException;
+import com.netflix.astyanax.connectionpool.Host;
 
 public class AstyanaxClient {
 
@@ -40,9 +81,53 @@ public class AstyanaxClient {
 	public static final String MAP_SIZE_DEFAULT = "crash: map_size needs to be set";
 
 	private static AstyanaxContext<Keyspace> context;
-	private static Keyspace keyspace;
-
+	private static Keyspace keyspace;	
+	private String latencyScoreStrategy;
 	public void init() {
 
+		ConnectionPoolConfigurationImpl connectionPoolConfig = new ConnectionPoolConfigurationImpl(SCORE_STRATEGY_DEFAULT);
+		latencyScoreStrategy = SCORE_STRATEGY_DEFAULT;
+			if(latencyScoreStrategy.equals("continuous")) {
+				connectionPoolConfig.setLatencyScoreStrategy(new EmaLatencyScoreContinuousStrategyImpl());
+			} else {
+				if(latencyScoreStrategy.equals("ema"))
+					connectionPoolConfig.setLatencyScoreStrategy(new EmaLatencyScoreStrategyImpl(100));
+				else
+					connectionPoolConfig.setLatencyScoreStrategy(new SmaLatencyScoreStrategyImpl());
+			}
+		context = new AstyanaxContext.Builder()
+					.forCluster("Test Cluster")
+					.forKeyspace("usertable")
+					.withAstyanaxConfiguration(
+							new AstyanaxConfigurationImpl()
+									.setDiscoveryType(
+											NodeDiscoveryType.valueOf(NODE_DISCOVERY_PROPERTY_DEFAULT))
+									.setConnectionPoolType(
+											ConnectionPoolType.valueOf(CONNECTION_POOL_PROPERTY_DEFAULT))
+									.setDefaultReadConsistencyLevel(
+											ConsistencyLevel.valueOf("CL_"+ READ_CONSISTENCY_LEVEL_PROPERTY_DEFAULT))
+									.setDefaultWriteConsistencyLevel(
+											ConsistencyLevel
+													.valueOf("CL_" + WRITE_CONSISTENCY_LEVEL_PROPERTY_DEFAULT))
+									.setCqlVersion("3.1.0").setTargetCassandraVersion("2.0"))
+					.withConnectionPoolConfiguration(
+							connectionPoolConfig
+									.setPort(
+											Integer.valueOf(PORT_PROPERTY_DEFAULT))
+									.setMaxConnsPerHost(
+											Integer.valueOf(MAXCONS_PROPERTY_DEFAULT))
+									.setSeeds(SEED_PROPERTY_DEFAULT)
+									.setHostSelectorStrategy(
+											HostSelectorStrategy.valueOf(HOST_SELECTOR_STRATEGY_DEFAULT)))
+					.withConnectionPoolMonitor(
+							new CountingConnectionPoolMonitor())
+					.buildKeyspace(ThriftFamilyFactory.getInstance());
+			
+			context.start();
+			keyspace = context.getEntity();
+
+			EMP_CF = ColumnFamily.newColumnFamily(EMP_CF_NAME,
+					StringSerializer.get(), StringSerializer.get());
 	}
+
 }

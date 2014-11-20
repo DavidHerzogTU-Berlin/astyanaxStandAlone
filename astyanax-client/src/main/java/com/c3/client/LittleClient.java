32b9ee03be5518be7734902a602ee46eff0eff6c
freecookie <david.herzog@campus.tu-berlin.de>
2014-11-20 13:45:45 +0100

    LittleClient now can talk to server

--- astyanax-client/src/main/java/com/c3/client/LittleClient.java
+++ astyanax-client/src/main/java/com/c3/client/LittleClient.java
@@ -1,5 +1,44 @@
 package com.c3.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class LittleClient {
-	
+public static void main(String[] args) throws IOException {
+        
+        String serverIP = "127.0.0.1";
+        int portNumber = 2345;
+
+        try (
+            Socket socket = new Socket(serverIP, portNumber);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);    
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Me: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about server " + serverIP);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                serverIP);
+            System.exit(1);
+        }
+    }
 }

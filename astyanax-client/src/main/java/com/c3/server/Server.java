873533217f0d811b7bca44df4dd68b68d8cf966a
freecookie <david.herzog@campus.tu-berlin.de>
2014-11-20 15:46:59 +0100

    Server now needs port as argument to run

--- astyanax-client/src/main/java/com/c3/server/Server.java
+++ astyanax-client/src/main/java/com/c3/server/Server.java
@@ -5,17 +5,22 @@ import java.net.ServerSocket;
 
 public class Server {
 	public static void main(String[] args) throws IOException {
-
-	        int portNumber = 2345;
-	        boolean listening = true;
-	        System.out.println("Server is running and listening...");
-	        try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
-	            while (listening) {
-		            new ServerThread(serverSocket.accept()).start();
-		        }
-		    } catch (IOException e) {
-	            System.err.println("Could not listen on port " + portNumber);
-	            System.exit(-1);
-	        }
-	    }
+		
+		if (args.length != 1) {
+			System.err.println("Usage (args): <port number>");
+			System.exit(1);
+		}
+		
+		int portNumber = Integer.parseInt(args[0]);
+		boolean listening = true;
+		System.out.println("Server is running and listening...");
+		try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+			while (listening) {
+				new ServerThread(serverSocket.accept()).start();
+			}
+		} catch (IOException e) {
+			System.err.println("Could not listen on port " + portNumber);
+			System.exit(-1);
+		}
+	}
 }

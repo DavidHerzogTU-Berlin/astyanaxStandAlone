8e80d4ea1e3e080009b5e9874a726a3f98131bb2
freecookie <david.herzog@campus.tu-berlin.de>
2014-11-25 18:30:45 +0100

    ServerThread now using | and § as separators

--- astyanax-client/src/main/java/com/c3/server/ServerThread.java
+++ astyanax-client/src/main/java/com/c3/server/ServerThread.java
@@ -20,7 +20,7 @@ public class ServerThread extends Thread{
     private HashMap<String, String> makeReadValues(String[] inputCommand) {
     	HashMap<String, String> values = new HashMap<String, String>();
     	for(int i = 2; i < inputCommand.length; i++) {
-    		String[] valuePair = inputCommand[i].split("-");
+    		String[] valuePair = inputCommand[i].split("§");
     		if (valuePair.length == 2)
     			values.put(valuePair[0], valuePair[1]);
     		else 
@@ -41,13 +41,20 @@ public class ServerThread extends Thread{
             AstyanaxClient ac = new AstyanaxClient();
             if(ac.init()) {
             	out.println("AstyanxClient INIT success.");
-            	HashMap<String, String> result = new HashMap<String, String>();
+            	
             	 while ((inputLine = in.readLine()) != null) {
-            		 String [] inputCommand = inputLine.split(";");
+            		 String [] inputCommand = inputLine.split("\\|");
             		 if(inputCommand.length >= 2 ) {
             			 if (inputCommand[0].equals("read")) {
-            				if( ac.read(null, inputCommand[1], null, result))
-            					out.println("read:success");
+            				HashMap<String, String> result = new HashMap<String, String>();
+            				if( ac.read(null, inputCommand[1], null, result)) {
+            					String readResult = "";
+            					for (String column : result.keySet()) {
+            						readResult = readResult + "Column: " + column + " Value: " + result.get(column) + " | ";
+            					}
+            					out.println("read:success: result: " + readResult);
+            				}
+            					
             				else 
             					out.println("read:failor");
             			 } else {

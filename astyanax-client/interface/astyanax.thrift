2c72bdc9e4b0ad8ab6f206553bd90f09123bc601
freecookie <david.herzog@campus.tu-berlin.de>
2014-11-29 18:02:31 +0100

    renamed variable in astyanax.thrift

--- astyanax-client/interface/astyanax.thrift
+++ astyanax-client/interface/astyanax.thrift
@@ -3,6 +3,6 @@ namespace java org.apache.astyanax.thrift
 service TalkToCassandraWithAstyanaxC3
 {
 	bool init(),
-	bool write(1:string key, 2:map<string, string> hashmap),
+	bool write(1:string key, 2:map<string, string> values),
 	map<string, string> read(1:string key, 2:set<string> fields),
 }
